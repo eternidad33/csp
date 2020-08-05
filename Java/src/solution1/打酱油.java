e64d2cc446f83d94d58530448692b444a7a225fb
vigilr <3496655347@qq.com>
2020-08-05 22:01:44 +0800

    156ms	21.28MB

--- Java/src/solution1/打酱油.java
+++ Java/src/solution1/打酱油.java
@@ -8,11 +8,11 @@ import java.util.Scanner;
  */
 public class 打酱油 {
     public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
-        int n=scanner.nextInt();
-        int a=n/50;
-        int b=(n-a*50)/30;
-        int c=(n-a*50-b*30)/10;
-        System.out.println(a*7+b*4+c);
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int a = n / 50;
+        int b = (n - a * 50) / 30;
+        int c = (n - a * 50 - b * 30) / 10;
+        System.out.println(a * 7 + b * 4 + c);
     }
 }
