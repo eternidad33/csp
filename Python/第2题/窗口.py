9b59cde19e2614e2673b987d45064933bbf33a47
vigilr <3496655347@qq.com>
2020-08-16 20:19:25 +0800

    100

--- Python/第2题/窗口.py
+++ Python/第2题/窗口.py
@@ -1,3 +1,21 @@
+def chuangkou(window, point_x, point_y):
+    """识别窗口"""
+    global i, j
+    find = False
+    for i, s in reversed(list(enumerate(window))):
+        if s[0] <= point_x <= s[2] and s[1] <= point_y <= s[3]:
+            print(s[4])
+            last = window[i]
+            for j in range(i, len(window) - 1):
+                window[j] = window[j + 1]
+            window[-1] = last
+            find = True
+            break
+    if not find:
+        print('IGNORED')
+
+
+# 录入数据
 n, m = map(int, input().split())
 w = []
 for i in range(n):
@@ -5,15 +23,4 @@ for i in range(n):
     w[i].append(i + 1)
 for j in range(m):
     x, y = list(map(int, input().split()))
-    find = False
-    for i, s in reversed(list(enumerate(w))):
-        if s[0] <= x <= s[2] and s[1] <= y <= s[3]:
-            print(s[4])
-            last = w[i]
-            for j in range(i, len(w) - 1):
-                w[j] = w[j + 1]
-            w[-1] = last
-            find = True
-            break
-    if not find:
-        print('IGNORED')
+    chuangkou(w, x, y)
