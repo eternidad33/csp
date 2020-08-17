e21fc7740fa0afdc4274464193f98eacfcd95fc1
vigilr <3496655347@qq.com>
2020-08-17 10:54:30 +0800

    100

--- Python/第1题/小明上学.py
+++ Python/第1题/小明上学.py
@@ -1,12 +1,16 @@
-r, y, g = list(map(int, input().split()))
-n = int(input())
-second = 0
-for i in range(n):
-    k, t = list(map(int, input().split()))
-    if k == 0 or k == 1:
-        second += t
-    elif k == 2:
-        second += (t + r)
-    else:
-        pass
-print(second)
+def xiaomingshangxue():
+    r, y, g = list(map(int, input().split()))
+    n = int(input())
+    second = 0
+    for i in range(n):
+        k, t = list(map(int, input().split()))
+        if k == 0 or k == 1:
+            second += t
+        elif k == 2:
+            second += (t + r)
+        else:
+            pass
+    print(second)
+
+
+xiaomingshangxue()
