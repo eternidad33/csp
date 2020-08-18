cb70ac962eaaa7cdd6562ca0fc0615d468ef22c5
vigilr <3496655347@qq.com>
2020-08-18 18:26:03 +0800

    100

--- Python/第2题/画图.py
+++ Python/第2题/画图.py
@@ -1,11 +1,16 @@
-n = int(input())
-pnum = [[0] * 100 for _ in range(100)]
-for i in range(n):
-    x1, y1, x2, y2 = map(int, input().split())
-    for x in range(x1, x2):
-        for y in range(y1, y2):
-            pnum[x][y] = 1
-area = 0
-for i in range(100):
-    area += sum(pnum[i])
-print(area)
+def huaTu():
+    n = int(input())
+    pnum = [[0] * 100 for _ in range(100)]
+    for i in range(n):
+        x1, y1, x2, y2 = map(int, input().split())
+        for x in range(x1, x2):
+            for y in range(y1, y2):
+                # 将被覆盖的区域置为1
+                pnum[x][y] = 1
+    area = 0
+    for i in range(100):
+        area += sum(pnum[i])
+    print(area)
+
+
+huaTu()
