1074c9a8c2281189380d1754201f2245fa904e1b
vigilr <3496655347@qq.com>
2020-08-19 19:36:49 +0800

    100

--- Python/第2题/日期计算.py
+++ Python/第2题/日期计算.py
@@ -1,14 +1,19 @@
+def riQiJiSuan(year, day):
+    mon = 0
+    md = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    # 判断闰年
+    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+        md[2] = 29
+    for i in md:
+        if day - i > 0:
+            day -= i
+            mon += 1
+        else:
+            break
+    print(mon)
+    print(day)
+
+
 y = int(input())
 d = int(input())
-mon = 0
-md = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-if (y % 4 == 0 and y % 100 != 0) or (y % 400 == 0):
-    md[2] = 29
-for i in md:
-    if d - i > 0:
-        d -= i
-        mon += 1
-    else:
-        break
-print(mon)
-print(d)
+riQiJiSuan(y, d)
