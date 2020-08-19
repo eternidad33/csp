211f37ae05ff60b5629b56862f129ae26530d295
vigilr <3496655347@qq.com>
2020-08-19 19:31:59 +0800

    100

--- Python/第2题/数字排序.py
+++ Python/第2题/数字排序.py
@@ -1,10 +1,16 @@
+def shuZiPaiXu(nums):
+    nums.sort()
+    a = {}
+    for i in nums:
+        a[i] = nums.count(i)
+    # 排序
+    a = sorted(a.items(), key=lambda item: item[1], reverse=True)
+    for j in a:
+        print(j[0], j[1])
+
+
 n = int(input())
 ls = list(map(int, input().split()))
-ls.sort()
-a = {}
-for i in ls:
-    a[i] = ls.count(i)
-a = sorted(a.items(), key=lambda item: item[1], reverse=True)
-for j in a:
-    print(j[0], j[1])
+shuZiPaiXu(ls)
+# 12
 # 5 2 3 3 1 3 4 2 5 2 3 5
