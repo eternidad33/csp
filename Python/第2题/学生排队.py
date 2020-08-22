b554a1723258e6f93238034ee65b51ce2db6e19a
vigilr <3496655347@qq.com>
2020-08-22 11:20:09 +0800

    100

--- Python/第2题/学生排队.py
+++ Python/第2题/学生排队.py
@@ -1,9 +1,13 @@
-n = int(input())
-q = list(range(1, n + 1))
-m = int(input())
-for i in range(m):
-    k, t = map(int, input().split())
-    after = q.index(k) + t
-    q.remove(k)
-    q.insert(after, k)
-print(' '.join(map(str, q)))
+def 学生排队():
+    n = int(input())
+    q = list(range(1, n + 1))
+    m = int(input())
+    for i in range(m):
+        k, t = map(int, input().split())
+        after = q.index(k) + t  # 移动后的索引
+        q.remove(k)  # 删除
+        q.insert(after, k)  # 插入
+    print(' '.join(map(str, q)))
+
+
+学生排队()
