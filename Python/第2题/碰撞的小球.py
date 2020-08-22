017c828917806256791f1b237b30f96b3de17ab2
vigilr <3496655347@qq.com>
2020-08-22 16:24:59 +0800

    100

--- Python/第2题/碰撞的小球.py
+++ Python/第2题/碰撞的小球.py
@@ -1,16 +1,21 @@
+def 碰撞的小球(count, axis_len, time, position):
+    dire = [1] * count  # 存储运动方向
+    for ti in range(time):
+        for p in range(count):
+            if position[p] == axis_len:
+                dire[p] = -1
+            if position[p] == 0:
+                dire[p] = 1
+            position[p] += dire[p]
+        # 查看小球是否相撞
+        for p in range(count):
+            for q in range(p + 1, count):
+                if position[p] == position[q]:
+                    dire[p] = -dire[p]
+                    dire[q] = -dire[q]
+    print(" ".join(str(i) for i in position))
+
+
 n, L, t = map(int, input().split())
-POS = list(map(int, input().split()))
-dire = [1] * n
-for ti in range(t):
-    for i in range(n):
-        if POS[i] == L:
-            dire[i] = -1
-        if POS[i] == 0:
-            dire[i] = 1
-        POS[i] += dire[i]
-    for i in range(n):
-        for j in range(i + 1, n):
-            if POS[i] == POS[j]:
-                dire[i] = -dire[i]
-                dire[j] = -dire[j]
-print(" ".join(str(i) for i in POS))
+POS = list(map(int, input().split()))  # 存储每个小球位置
+碰撞的小球(n, L, t, POS)
