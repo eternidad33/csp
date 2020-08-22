87b7d92c8017dec578f9533bb1fbf9a3bdc829f4
vigilr <3496655347@qq.com>
2020-08-22 16:30:53 +0800

    100

--- Python/第2题/买菜.py
+++ Python/第2题/买菜.py
@@ -1,15 +1,20 @@
-n = int(input())
-H, W = [], []
-for i in range(n):
-    H.append(list(map(int, input().split())))
-for i in range(n):
-    W.append(list(map(int, input().split())))
-tlen = max(H[-1][-1], W[-1][-1])
-timeLine = [0] * tlen
-for s, e in H:
-    for i in range(s, e):
-        timeLine[i] += 1
-for s, e in W:
-    for i in range(s, e):
-        timeLine[i] += 1
-print(timeLine.count(2))
+def 买菜():
+    n = int(input())
+    H, W = [], []
+    for i in range(n):
+        H.append(list(map(int, input().split())))
+    for i in range(n):
+        W.append(list(map(int, input().split())))
+    tlen = max(H[-1][-1], W[-1][-1])
+    # 初始化时间轴
+    timeLine = [0] * tlen
+    for s, e in H:
+        for i in range(s, e):
+            timeLine[i] += 1
+    for s, e in W:
+        for i in range(s, e):
+            timeLine[i] += 1
+    print(timeLine.count(2))
+
+
+买菜()
