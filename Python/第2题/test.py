b736cf5dc98cc1b36ccb67ba9906af7858f9af90
vigilr <3496655347@qq.com>
2020-08-22 11:52:33 +0800

    100

--- Python/第2题/test.py
+++ Python/第2题/test.py
@@ -1,39 +1,47 @@
-#!/usr/bin/python3
-# -*- coding:utf-8 -*-
-"""
-@author: Vigilr
-@file: test.py
-@date: 2020-08-18
-@Editor: PyCharm
-@desc: 
-"""
-n = int(input())
-pnum = []
-for i in range(n):
-    pnum.append(list(map(int, input().split())))
-i, j = 0, 0
-line = []
-up = True
-for t in range(n * n):
-    line.append(pnum[i][j])
-    if up:
-        if j == n - 1:
-            i += 1
-            up = False
-        elif i == 0:
-            j += 1
-            up = False
-        else:
-            j += 1
-            i -= 1
-    else:
-        if i == n - 1:
-            j += 1
-            up = True
-        elif j == 0:
-            i += 1
-            up = True
-        else:
-            j -= 1
-            i += 1
-print(" ".join(map(str, line)))
+# 公共钥匙盒
+
+# 找出要借，或者要还的钥匙编号
+def return_lend_key(info, time):
+    RL_key = []
+    for i in range(0, len(time)):
+        if time[i] == t:
+            RL_key.append(info[i][0])
+    return RL_key
+
+
+n, k = input().split()
+n = int(n)
+k = int(k)
+key = [i for i in range(1, n + 1)]  # 钥匙盒
+info = [[0 for i in range(3)] for j in range(k)]  # 存放钥匙使用情况
+
+for i in range(k):
+    info[i][:] = [int(wsc) for wsc in input().split()]
+
+for i in range(k):
+    info[i][2] = info[i][1] + info[i][2]
+
+start_time = [i[1] for i in info]  # 二维列表切片操作
+end_time = [i[2] for i in info]
+
+for t in range(1, max(end_time) + 1):
+    # 先还后借
+    # 还
+    if t in end_time:
+        return_key = return_lend_key(info, end_time)  # 找出要还的钥匙
+        return_key.sort()
+        for r in range(len(return_key)):
+            for i in range(n):
+                if key[i] == 0:
+                    key[i] = return_key[r]
+                    break
+    # 借
+    if t in start_time:
+        lend_key = return_lend_key(info, start_time)  # 找出要借的钥匙
+        for r in range(len(lend_key)):
+            for i in range(n):
+                if key[i] == lend_key[r]:
+                    key[i] = 0
+
+for i in key:
+    print(i, end=' ')
--- Python/第2题/公共钥匙盒.py
+++ Python/第2题/公共钥匙盒.py
@@ -1,32 +1,35 @@
-n, k = map(int, input().split())
-key = list(range(1, n + 1))
-info = []
-for i in range(k):
-    info.append(list(map(int, input().split())))
-
-start_time = [i[1] for i in info]
-end_time = [i[2] for i in info]
-
-
-def return_lend_key(info, time):
+def return_lend_key(key_info, times, time):
     RL_key = []
-    for i in range(len(time)):
-        if time[i] == t:
-            RL_key.append(info[i][0])
+    for i in range(len(times)):
+        if times[i] == time:
+            RL_key.append(key_info[i][0])
     return RL_key
 
 
+n, k = map(int, input().split())
+info = []
+for i in range(k):
+    info.append(list(map(int, input().split())))
+key = list(range(1, n + 1))
+
+# 分别存储开始使用时间和归还时间
+start_time = [i[1] for i in info]
+end_time = [i[1] + i[2] for i in info]
+
 for t in range(1, max(end_time) + 1):
+    # 先还后借
+    # t时刻为归还时间
     if t in end_time:
-        return_key = return_lend_key(info, end_time)
+        return_key = return_lend_key(info, end_time, t)
         return_key.sort()
         for r in return_key:
             for i in range(n):
                 if key[i] == 0:
                     key[i] = r
                     break
+    # t时刻为借用时间
     if t in start_time:
-        lend_key = return_lend_key(info, start_time)
+        lend_key = return_lend_key(info, start_time, t)
         for r in lend_key:
             for i in range(n):
                 if key[i] == r:
