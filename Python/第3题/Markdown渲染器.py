a31cfd1ec7648c59b86f50ab108eecabdd968020
vigilr <3496655347@qq.com>
2020-09-10 23:00:23 +0800

    0

---
+++ Python/第3题/Markdown渲染器.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+"""
+@author: Vigilr
+@file: Markdown渲染器.py
+@date: 2020-09-10
+@Editor: PyCharm
+@desc: 
+"""
+import sys
+
+
+def Markdown渲染器(lc, s0):
+    pass
+
+
+w = int(input())
+s = []
+for line in sys.stdin:
+    s.append(line)
+Markdown渲染器(w, s)
